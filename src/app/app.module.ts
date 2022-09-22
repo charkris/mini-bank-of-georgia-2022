401bf32125cbadff5d502bd771898d8eee5a3ac6
Kristina <k_charkviani@cu.edu.ge>
2022-09-22 20:10:44 +0400

    basic structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,20 @@ import { AuthComponent } from './auth/auth.component';
 import { ShellComponent } from './shell/shell.component';
 import { ShellHeaderComponent } from './shell/shell-header/shell-header.component';
 import { ShellSidebarComponent } from './shell/shell-sidebar/shell-sidebar.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { SharedComponent } from './shared/shared.component';
+import { ClientHeaderComponent } from './shell/client-header/client-header.component';
+import { BpmComponent } from './shell/modules/bpm/bpm.component';
+import { Bpm000Component } from './shell/modules/bpm/bpm000/bpm000.component';
+import { Bpm001Component } from './shell/modules/bpm/bpm001/bpm001.component';
+import { KrnComponent } from './shell/modules/krn/krn.component';
+import { AccountsComponent } from './shell/modules/krn/accounts/accounts.component';
+import { KrnicpComponent } from './shell/modules/krn/krnicp/krnicp.component';
+import { OperationsComponent } from './shell/modules/krn/operations/operations.component';
+import { CreateAccountComponent } from './shell/modules/krn/accounts/create-account/create-account.component';
+import { PmdComponent } from './shell/modules/pmd/pmd.component';
+import { Pmd311Component } from './shell/modules/pmd/pmd311/pmd311.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +29,21 @@ import { ShellSidebarComponent } from './shell/shell-sidebar/shell-sidebar.compo
     AuthComponent,
     ShellComponent,
     ShellHeaderComponent,
-    ShellSidebarComponent
+    ShellSidebarComponent,
+    LoginComponent,
+    RegisterComponent,
+    SharedComponent,
+    ClientHeaderComponent,
+    BpmComponent,
+    Bpm000Component,
+    Bpm001Component,
+    KrnComponent,
+    AccountsComponent,
+    KrnicpComponent,
+    OperationsComponent,
+    CreateAccountComponent,
+    PmdComponent,
+    Pmd311Component
   ],
   imports: [
     BrowserModule,
