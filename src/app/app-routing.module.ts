e99e9d81cc9a9825e370f85e5aa025198142603a
Kristina <k_charkviani@cu.edu.ge>
2022-10-01 02:28:43 +0400

    added some style

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,17 @@ import {RouterModule, Routes} from '@angular/router';
 import {AuthComponent} from './auth/auth.component';
 import {ShellComponent} from './shell/shell.component';
 import {RegisterComponent} from './auth/register/register.component';
+import {Bpm000Component} from './shell/modules/bpm/bpm000/bpm000.component';
+import {BpmComponent} from './shell/modules/bpm/bpm.component';
+import {Bpm001Component} from './shell/modules/bpm/bpm001/bpm001.component';
+import {KrnComponent} from './shell/modules/krn/krn.component';
+import {KrnicpComponent} from './shell/modules/krn/krnicp/krnicp.component';
+import {AccountsComponent} from './shell/modules/krn/accounts/accounts.component';
+import {CreateAccountComponent} from './shell/modules/krn/accounts/create-account/create-account.component';
+import {OperationsComponent} from './shell/modules/krn/operations/operations.component';
+import {LoginComponent} from './auth/login/login.component';
+import {PmdComponent} from './shell/modules/pmd/pmd.component';
+import {Pmd311Component} from './shell/modules/pmd/pmd311/pmd311.component';
 
 const routes: Routes = [
   {
@@ -10,17 +21,72 @@ const routes: Routes = [
     component: AuthComponent,
     children: [
       {
+        path: '',
+        component: LoginComponent,
+      },
+      {
         path: 'register',
-        component: AuthComponent,
+        component: RegisterComponent,
       }
     ]
   },
   {
     path: '',
     component: ShellComponent,
-    children: []
+    children: [
+      {
+        path: 'bpm',
+        component: BpmComponent,
+        children: [
+          {
+            path: 'bpm000',
+            component: Bpm000Component,
+          },
+          {
+            path: 'bpm001',
+            component: Bpm001Component,
+          }
+        ]
+      },
+      {
+        path: 'krn',
+        component: KrnComponent,
+        children: [
+          {
+            path: 'accounts',
+            component: AccountsComponent,
+            children: [
+              {
+                path: 'create',
+                component: CreateAccountComponent
+              }
+            ]
+          },
+          {
+            path: 'krnicp',
+            component: KrnicpComponent,
+          },
+          {
+            path: 'operations',
+            component: OperationsComponent
+          }
+
+        ]
+      },
+      {
+        path: 'pmd',
+        component: PmdComponent,
+        children: [
+          {
+            path: 'pmd311',
+            component: Pmd311Component,
+          }
+        ]
+      }
+    ],
   },
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {logger} from 'codelyzer/util/logger';
 
@@ -7,23 +7,23 @@ import {logger} from 'codelyzer/util/logger';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnChanges {
 
   constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
   }
 
-  onInput(inp) {
-    console.log((inp.target as HTMLInputElement).value);
-
-  }
-  goToRegister() {
-    console.log('register');
-    //console.log(this.activeRoute)
-    this.router.navigate(['/register']);
-    console.log(this.route.snapshot)
-  }
+  // onInput(inp) {
+  //   console.log((inp.target as HTMLInputElement).value);
+  //
+  // }
+  // goToRegister() {
+  //   console.log('register');
+  //   //console.log(this.activeRoute)
+  //   this.router.navigate(['/register']);
+  //   console.log(this.route.snapshot);
+  // }
 
   ngOnChanges(changes: SimpleChanges): void {
     console.log(changes);
